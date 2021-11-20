7d679e18d6fffa0c406cf80fafff69255f7e8b2c
Nuriddin <Nurik900707@gmail.com>
2021-11-20 19:33:52 +0500

    task 6

---
+++ recursion/juftSon.js
@@ -0,0 +1,11 @@
+function juftSon(n) {
+    if (n === 0) {
+        return `Siz nol kiritdingiz`;
+    }
+    else if (n % 2 === 0){
+        return true
+    }else {
+        return false
+    }
+}
+console.log(juftSon(0));
