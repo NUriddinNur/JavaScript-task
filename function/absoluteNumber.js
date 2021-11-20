bc6a5907e32cc9c6186ea5e50461444c0aa0121a
Nuriddin <Nurik900707@gmail.com>
2021-11-20 15:27:17 +0500

    task 5

---
+++ function/absoluteNumber.js
@@ -0,0 +1,11 @@
+function checkNumber(n) {
+
+    for (let i = 2; i <= n/2; i++) {
+        if (n % i === 0) {
+            return false
+        }
+        return true
+    }
+}
+
+console.log(checkNumber(4));
