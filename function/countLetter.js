daa5dee837a37cf88661f70e1a11de61ee53c056
Nuriddin <Nurik900707@gmail.com>
2021-11-22 23:57:56 +0500

    task 17

--- function/countLetter.js
+++ function/countLetter.js
@@ -1,18 +1,7 @@
-function countLetter(word, letter) {
-    word = word.split("")
-
-    for (let i of word){
-        let result = 0
-        if (i === undefined || i === " "){
-            continue
-        }
-        for (let j = 0; j < word.length; j++) {
-            if (i === word[j]){
-                result++;
-                delete word[j]
-            }
-        }
-        console.log(`${i} ${result} ta`);
-    }
+function countLetter(word, let) {
+    const res = word.split("");
+    return res.reduce((acc, currentValue) => {
+        currentValue === let ? acc++ : acc
+        return acc
+    },0)
 }
-countLetter("salom assalom", "o");
