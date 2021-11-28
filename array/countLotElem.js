fdaab46426a7f340e301de77a47cb0f944e2c284
Nuriddin <Nurik900707@gmail.com>
2021-11-28 18:13:36 +0500

    task 7

---
+++ array/countLotElem.js
@@ -0,0 +1,15 @@
+function countLotElem(arr) {
+    let count = 0;
+    let element = 0;
+
+    for (let i of arr) {
+        let foo = arr.filter(x => x === i).length
+        if (foo > count) {
+            element = i
+            count = foo
+        }
+    }
+    return {count, element}
+}
+
+console.log(countLotElem([1,2,3,1,1,2,2,2,3,3,3,3]));
